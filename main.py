e51a8f04588111e9faeb0b049045d81bcb119c43
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-05-06 23:22:56 -0600

    Config API Fredapi

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+from fredapi import Fred
+
 plt.style.use('fivethirtyeight')
+pd.set_option('max_columns', 500)
+color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 print('Economic Analysis')
